0caeba97b28d44e328a346821a3f746b8c73f8e6
TomokiTX57 <tomoki223d@gmail.com>
2025-04-14 14:17:18 +0000

    fix layout of checkout

--- resources/views/zero_systems/checkout.blade.php
+++ resources/views/zero_systems/checkout.blade.php
@@ -14,18 +14,18 @@
             @endif
     </div>
 
-    <div class="p-6 bg-white shadow rounded mt-4">
+    <div class="p-3 overflow-x-auto bg-white shadow rounded mt-4">
         <h3 class="text-lg font-semibold">本日の履歴</h3>
-        <table class="table table-bordered w-full mt-2">
+        <table class="table table-bordered table-striped text-xs w-full mt-2">
             <thead>
                 <tr>
-                    <th>日時</th>
-                    <th>チップ</th>
-                    <th>合計チップ</th>
-                    <th>種別</th>
-                    <th>処理</th>
-                    <th>会計番号</th>
-                    <th>コメント</th>
+                    <th class="whitespace-nowrap">日時</th>
+                    <th class="whitespace-nowrap">チップ</th>
+                    <th class="whitespace-nowrap">合計チップ</th>
+                    <th class="whitespace-nowrap">種別</th>
+                    <th class="whitespace-nowrap">処理</th>
+                    <th class="whitespace-nowrap">会計番号</th>
+                    <th class="whitespace-nowrap">コメント</th>
                 </tr>
             </thead>
             <tbody>
